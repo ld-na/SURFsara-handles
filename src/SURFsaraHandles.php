fb6f11ab9dc5d9d2a457f437b2506984f90d1d55
Bekebrede, Martijn <Martijn.Bekebrede@ordina.nl>
2016-09-02 15:47:39 +0200

    Fixed removed code in class.

--- src/SURFsaraHandles.php
+++ src/SURFsaraHandles.php
@@ -3,67 +3,136 @@
 namespace GravityDesignNL\SURFsara;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
+/**
+ * Class SURFsaraHandles.
+ *
+ * @package GravityDesignNL\SURFsara
+ */
 class SURFsaraHandles {
 
   /**
-   * Variables.
+   * The Guzzle HTTP client.
+   */
+  protected $client;
+
+  /**
+   * Validation messages.
+   */
+  protected $messages = [];
+
+  /**
+   * Are all settings valid?
+   */
+  protected $valid = TRUE;
+
+  /**
+   * Settings variables.
    *
    *   The configuration array must contain the following entries:
-   *     [key] - The full path to the private-key file.
-   *     [cert] - The full path to the certificate file.
-   *     [handle-name] - The handle name.
-   *     [handle-url] - The base handle url the PID redirects to.
-   *     [surfsara-url] - The SURFsara handle api url
-   *     [surfsara-prefix] - The SURFsara handle api prefix.
-   *     [overwrite] - 'true' of 'false', defaults to 'true'.
+   *     [key]               - The full path to the private-key file.
+   *     [cert]              - The full path to the certificate file.
+   *     [handleName]        - The handle name.
+   *     [handleUrl]         - The base handle url the PID redirects
+   *                           to.
+   *     [surfsaraApi]       - The SURFsara handle api url
+   *     [surfsaraPrefixOrg] - The SURFsara handle api prefix
+   *                           organisation code.
+   *     [surfsaraPrefixEnv] - The SURFsara handle api prefix
+   *                           environment code.
+   *     [permissions]       - The permissions needed to set/get a handle.
+   *     [overwrite]         - 'true' of 'false', defaults to 'true'.
+   *     [verify]            - The configuration verify parameter,
+   *                           defaults to FALSE.
+   *     [headers]           - The extra headers to be added to a
+   *                           request, defaults to empty array.
    */
   protected $key;
   protected $cert;
   protected $handleName;
   protected $handleUrl;
   protected $surfsaraApi;
-  protected $surfsaraPrefix;
+  protected $surfsaraPrefixOrg;
+  protected $surfsaraPrefixEnv;
+  protected $permissions;
   protected $overwrite = 'true';
+  protected $verify = FALSE;
+  protected $headers = [];
+
+  /**
+   * SURFsaraHandles constructor.
+   */
+  public function __construct() {
+    $this->client = new Client();
+  }
 
   /**
    * Set the handle.
    */
   public function setHandle() {
+    $response = [];
 
-    $json = [
-      'values' => [[
-        'index' => 100,
-        'type' => 'HS_ADMIN',
-        'data' => [
-          'value' => [
-            'index' => 200,
-            'handle' => '0.NA/1000',
-            'permissions' => '011111110011',
-            'format' => 'admin'
+    try {
+      if ($this->isValid()) {
+        $json = [
+          'values' => [
+            [
+              'index' => 1,
+              'type' => 'URL',
+              'data' => [
+                'format' => 'string',
+                'value' => $this->getHandleUrl(),
+              ],
+            ],
+            [
+              'index' => 100,
+              'type' => 'HS_ADMIN',
+              'data' => [
+                'format' => 'admin',
+                'value' => [
+                  'index' => 200,
+                  'handle' => $this->getSurfsaraPrefix(),
+                  'permissions' => $this->getPermissions(),
+                ],
+              ],
+            ],
           ],
-          'format' => 'admin',
-        ]
-      ],[
-        'index' => 1,
-        'type' => 'URL',
-        'data' => $this->getHandleUrl(),
-      ]]
-    ];
+        ];
 
-    $extra_headers = ['Authorization' => 'Handle clientCert="true"'];
-    $config = [
-      'headers' => $extra_headers,
-      'verify' => false,
-      'ssl_key' => $this->getKey(),
-      'cert' => $this->getCert(),
-      'json' => $json,
-    ];
+        $config = [
+          'headers' => $this->getHeaders(),
+          'verify' => $this->isVerify(),
+          'ssl_key' => $this->getKey(),
+          'cert' => $this->getCert(),
+          'json' => $json,
+        ];
 
-    $url = $this->getSurfsaraUrl();
-    $client = new Client();
+        try {
+          $result = $this->client->put($this->getSurfsaraUrl(), $config);
+          $response['result'] = (in_array($result->getStatusCode(), [200, 201])) ? TRUE : FALSE;
+          $response['code'] = $result->getStatusCode();
+          $response['message'] = $result->getReasonPhrase();
+        }
+        catch (ClientException $exception) {
+          // Return FALSE if handle can't be set because of a failing
+          // connection.
+          $response['result'] = FALSE;
+          $response['code'] = $exception->getCode();
+          $response['message'] = $exception->getMessage();
+        }
+      }
+    }
+    catch (\Exception $invalid) {
+      // Return FALSE if handle can't be set because of missing or
+      // invalid settings.
+      $response['result'] = FALSE;
+      $response['code'] = $invalid->getCode();
+      $response['message'] = $invalid->getMessage();
+    }
 
-    return $client->put($url, $config);
+    // Return response.
+    return $response;
   }
 
   /**
@@ -71,123 +140,467 @@ class SURFsaraHandles {
    */
   public function deleteHandle() {
 
-    $url = $this->getSurfsaraUrl();
-    $client = new Client();
+    try {
+      $result = $this->client->delete($this->getSurfsaraUrl());
+      $response['result'] = ($result->getStatusCode() == 200) ? TRUE : FALSE;
+      $response['code'] = $result->getStatusCode();
+      $response['message'] = $result->getReasonPhrase();
+    }
+    catch (ClientException $exception) {
+      // Return FALSE if handle can't be deleted because of a failing
+      // connection.
+      $response['result'] = FALSE;
+      $response['code'] = $exception->getCode();
+      $response['message'] = $exception->getMessage();
+    }
 
-    return $client->delete($url);
+  }
+
+  /**
+   * Check if all input is available and valid.
+   *
+   * @throws \Exception
+   *   Throws message why the settings are not valid.
+   */
+  public function isValid() {
+    // Run validations.
+    $this->isValidKey();
+    $this->isValidCert();
+    $this->isValidHandleName();
+    $this->isValidHandleUrl();
+    $this->isValidSurfsaraApi();
+    $this->isValidSurfsaraPrefixOrg();
+    $this->isValidSurfsaraPrefixEnv();
+    $this->isValidOverwrite();
+    $this->isValidPermissions();
+    $this->isValidVerify();
+    $this->isValidHeaders();
+
+    if ($this->getValid() === FALSE) {
+      throw new \Exception('Your settings are not valid. Please check the messages or contact your administrator.', 9000);
+    }
+
+    return $this->getValid();
   }
 
   /**
    * Compose and return the full SURFsara url.
    *
-   * @return mixed|string
+   * @return string $url
+   *   The composite url to the SURFsara environment.
    */
-  public function getSurfsaraUrl() {
-
+  private function getSurfsaraUrl() {
     $api = $this->getSurfsaraApi();
-    $prefix = $this->getSurfsaraPrefix();
+    $prefix = $this->getSurfsaraPrefixEnv();
     $name = $this->getHandleName();
-    $overwrite = $this->getOverwrite();
-    return $api . '/' . $prefix . '/' . $name . '?overwrite=' . $overwrite;
+    $url = $api . '/' . $prefix . '/' . $name;
+    if (!empty($this->getOverwrite())) {
+      $url = $url . '?overwrite=' . $this->getOverwrite();
+    }
+    return $url;
   }
 
+  /**
+   * Return the full SURFsara prefix.
+   *
+   * @return string
+   *   The SURFsara prefix.
+   */
+  private function getSurfsaraPrefix() {
+    return $this->getSurfsaraPrefixOrg() . '/' . $this->getSurfsaraPrefixEnv();
+  }
 
   /**
-   * @return mixed
+   * Get the full path to the private-key file.
+   *
+   * @return string
+   *   The full path to the private-key file.
    */
   public function getKey() {
     return $this->key;
   }
 
   /**
-   * @param mixed $key
+   * Set the full path to the private-key file.
+   *
+   * @param string $key
+   *   The full path to the private-key file.
    */
   public function setKey($key) {
     $this->key = $key;
   }
 
   /**
-   * @return mixed
+   * Check if key is available and valid.
+   */
+  private function isValidKey() {
+    if (empty($this->getKey())) {
+      $this->setMessage(new \Exception('The path to the private-key file can not be empty', 9001));
+      $this->setInvalid();
+    }
+    if (!file_exists($this->getKey())) {
+      $this->setMessage(new \Exception('The private-key file can not be found.', 9002));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the full path to the certificate file.
+   *
+   * @return string
+   *   The full path to the certificate file.
    */
   public function getCert() {
     return $this->cert;
   }
 
   /**
-   * @param mixed $cert
+   * Set the full path to the certificate file.
+   *
+   * @param string $cert
+   *   The full path to the certificate file.
    */
   public function setCert($cert) {
     $this->cert = $cert;
   }
 
   /**
-   * @return mixed
+   * Check if cert is available and valid.
+   */
+  private function isValidCert() {
+    if (empty($this->getCert())) {
+      $this->setMessage(new \Exception('The path to the sertificate file can not be empty', 9003));
+      $this->setInvalid();
+    }
+    if (!file_exists($this->getCert())) {
+      $this->setMessage(new \Exception('The certificate file can not be found.', 9004));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the handle name.
+   *
+   * @return string
+   *   The handle name.
    */
   public function getHandleName() {
     return $this->handleName;
   }
 
   /**
-   * @param mixed $handleName
+   * Set the handle name.
+   *
+   * @param string $handleName
+   *   The handle name.
    */
   public function setHandleName($handleName) {
     $this->handleName = $handleName;
   }
 
   /**
-   * @return mixed
+   * Check if handle name is available and valid.
+   */
+  private function isValidHandleName() {
+    if (empty($this->getHandleName())) {
+      $this->setMessage(new \Exception('The handle name can not be empty', 9005));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the base handle url the PID redirects to.
+   *
+   * @return string
+   *   The base handle url the PID redirects to.
    */
   public function getHandleUrl() {
     return $this->handleUrl;
   }
 
   /**
-   * @param mixed $handleUrl
+   * Set the base handle url the PID redirects to.
+   *
+   * @param string $handleUrl
+   *   The base handle url the PID redirects to.
    */
   public function setHandleUrl($handleUrl) {
     $this->handleUrl = $handleUrl;
   }
 
   /**
-   * @return mixed
+   * Check if handle url is available and valid.
+   */
+  private function isValidHandleUrl() {
+    if (empty($this->getHandleUrl())) {
+      $this->setMessage(new \Exception('The handle url can not be empty', 9006));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the SURFsara handle api url.
+   *
+   * @return string
+   *   The SURFsara handle api url.
    */
   public function getSurfsaraApi() {
     return $this->surfsaraApi;
   }
 
   /**
-   * @param mixed $surfsaraApi
+   * Set the SURFsara handle api url.
+   *
+   * @param string $surfsara_api
+   *   The SURFsara handle api url.
    */
-  public function setSurfsaraApi($surfsaraApi) {
-    $this->surfsaraApi = $surfsaraApi;
+  public function setSurfsaraApi($surfsara_api) {
+    $this->surfsaraApi = $surfsara_api;
   }
 
   /**
-   * @return mixed
+   * Check if SURFsara API url is available and valid.
    */
-  public function getSurfsaraPrefix() {
-    return $this->surfsaraPrefix;
+  private function isValidSurfsaraApi() {
+    if (empty($this->getSurfsaraApi())) {
+      $this->setMessage(new \Exception('The SURFsara api url can not be empty', 9007));
+      $this->setInvalid();
+    }
   }
 
   /**
-   * @param mixed $surfsaraPrefix
+   * Get the SURFsara handle api organisation prefix.
+   *
+   * @return string
+   *   The SURFsara handle api organisation prefix.
    */
-  public function setSurfsaraPrefix($surfsaraPrefix) {
-    $this->surfsaraPrefix = $surfsaraPrefix;
+  public function getSurfsaraPrefixOrg() {
+    return $this->surfsaraPrefixOrg;
   }
 
   /**
-   * @return mixed|string
+   * Set the SURFsara handle api organisation prefix.
+   *
+   * @param string $surfsara_prefix_organisation
+   *   The SURFsara handle api organisation prefix.
+   */
+  public function setSurfsaraPrefixOrg($surfsara_prefix_organisation) {
+    $this->surfsaraPrefixOrg = $surfsara_prefix_organisation;
+  }
+
+  /**
+   * Check if SURFsara organisation prefix is available and valid.
+   */
+  private function isValidSurfsaraPrefixOrg() {
+    if (empty($this->getSurfsaraPrefixOrg())) {
+      $this->setMessage(new \Exception('The organisation prefix can not be empty', 9008));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the SURFsara handle api environment prefix.
+   *
+   * @return string
+   *   The SURFsara handle api environment prefix.
+   */
+  public function getSurfsaraPrefixEnv() {
+    return $this->surfsaraPrefixEnv;
+  }
+
+  /**
+   * Set the SURFsara handle api environment prefix.
+   *
+   * @param string $surfsara_prefix_environment
+   *   The SURFsara handle api environment prefix.
+   */
+  public function setSurfsaraPrefixEnv($surfsara_prefix_environment) {
+    $this->surfsaraPrefixEnv = $surfsara_prefix_environment;
+  }
+
+  /**
+   * Check if SURFsara environment prefix is available and valid.
+   */
+  private function isValidSurfsaraPrefixEnv() {
+    if (empty($this->getSurfsaraPrefixEnv())) {
+      $this->setMessage(new \Exception('The environment prefix can not be empty', 9009));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the permissions value.
+   *
+   * @return string
+   *   The permissions value;
+   */
+  public function getPermissions() {
+    return $this->permissions;
+  }
+
+  /**
+   * Set the permissions value.
+   *
+   * @param string $permissions
+   *   The permissions value.
+   */
+  public function setPermissions($permissions) {
+    $this->permissions = $permissions;
+  }
+
+  /**
+   * Check if permissions are available and valid.
+   */
+  private function isValidPermissions() {
+    if (empty($this->getPermissions())) {
+      $this->setMessage(new \Exception('The permissions can not be empty', 9010));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the overwrite value.
+   *
+   * @return string
+   *   The overwrite value.
    */
   public function getOverwrite() {
     return $this->overwrite;
   }
 
   /**
-   * @param mixed|string $overwrite
+   * Set the overwrite value.
+   *
+   * @param string $overwrite
+   *   The overwrite value to 'true' of 'false'.
    */
   public function setOverwrite($overwrite) {
     $this->overwrite = $overwrite;
+  }
+
+  /**
+   * Check if overwrite is available and valid.
+   */
+  private function isValidOverwrite() {
+    $valid_options = ['true', 'false'];
+    if (!empty($this->getOverwrite()) && !in_array($this->getOverwrite(), $valid_options)) {
+      $this->setMessage(new \Exception('The overwrite should be a string set to either "true" or "false".', 9011));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the verify value.
+   *
+   * @return bool
+   *   The verify value.
+   */
+  public function isVerify() {
+    return $this->verify;
+  }
+
+  /**
+   * Set the verify value.
+   *
+   * @param bool $verify
+   *   The verify value.
+   */
+  public function setVerify($verify) {
+    $this->verify = $verify;
+  }
+
+  /**
+   * Check if verify is available and valid.
+   */
+  private function isValidVerify() {
+    if (!is_bool($this->isVerify())) {
+      $this->setMessage(new \Exception('The verify should be a boolean set to either "true" or "false".', 9012));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the extra headers to be added to a request.
+   *
+   * @return array
+   *   The extra headers to be added to a request.
+   */
+  public function getHeaders() {
+    return $this->headers;
+  }
+
+  /**
+   * Set the extra headers to be added to a request.
+   *
+   * @param array $headers
+   *   The extra headers to be added to a request.
+   */
+  public function setHeaders($headers) {
+    $this->headers = $headers;
+  }
+
+  /**
+   * Check if headers is available and valid.
+   */
+  private function isValidHeaders() {
+    if (!is_array($this->getHeaders())) {
+      $this->setMessage(new \Exception('The headers should be set in an array.', 9013));
+      $this->setInvalid();
+    }
+  }
+
+  /**
+   * Get the messages.
+   *
+   * @return array
+   *   The collected messages.
+   */
+  public function getMessages() {
+    return $this->messages;
+  }
+
+  /**
+   * Set a message.
+   *
+   * @param \Exception $message
+   *   An exception containing a code and a message.
+   */
+  private function setMessage(\Exception $message) {
+    $this->messages[] = $message;
+  }
+
+  /**
+   * Clear the messages array.
+   */
+  public function clearMessages() {
+    $this->messages = [];
+  }
+
+  /**
+   * Get validation status.
+   *
+   * @return bool
+   *   The validation status.
+   */
+  public function getValid() {
+    return $this->valid;
+  }
+
+  /**
+   * Set validation status.
+   *
+   * @param bool $valid
+   *   The validation status.
+   */
+  public function setValid($valid = TRUE) {
+    $this->valid = $valid;
+  }
+
+  /**
+   * Set validation status to FALSE.
+   */
+  public function setInvalid() {
+    $this->setValid(FALSE);
   }
 
 }
